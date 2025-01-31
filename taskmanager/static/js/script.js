c5140cbfd96cd5364c851d3b6c8f89acf6c97dff
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2025-01-31 21:33:03 +0000

    Added page structure with nav, body, main and footer elements, incorporated template inheritance

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialisation
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
